3eeff0bdd261f7e476591ff7b6d8c22d371897f6
pratiktamhane <96224999+pratiktamhane@users.noreply.github.com>
2022-03-20 11:52:13 +0530

    Amazon-Prime-Trending

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,81 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = (props) => {
+  const [index, setIndex] = useState(0);
+  const [data, setdata] = useState(props.slides[2]);
+
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-testid="title">{props.slides[index].title}</h1>
+      <p data-testid="text">{props.slides[index].text}</p>
+      {index === 0 ? (
+        <button
+          data-testid="button-prev"
+          disabled={true}
+          // style={{ display: "none", }}
+          onClick={() => {
+            setIndex(index - 1);
+            // setdata(props.slides[index]);
+          }}
+        >
+          Prev
+        </button>
+      ) : (
+        <button
+          data-testid="button-prev"
+          onClick={() => {
+            setIndex(index - 1);
+            // setdata(props.slides[index]);
+          }}
+        >
+          Prev
+        </button>
+      )}
+      {index === 4 ? (
+        <button
+          disabled={true}
+          data-testid="button-next"
+          // style={{ display: "none" }}
+          onClick={() => {
+            setIndex(index + 1);
+            // setdata(props.slides[index]);
+          }}
+        >
+          Next
+        </button>
+      ) : (
+        <button
+          data-testid="button-next"
+          onClick={() => {
+            setIndex(index + 1);
+            // setdata(props.slides[index]);
+          }}
+        >
+          Next
+        </button>
+      )}
+      {index === 0 ? (
+        <button
+          data-testid="button-restart"
+          //  style={{ display: "none" }}
+          disabled={true}
+        >
+          Restart
+        </button>
+      ) : (
+        <button
+          data-testid="button-restart"
+          onClick={() => {
+            setIndex(0);
+            // setdata(props.slides[index]);
+          }}
+        >
+          Restart
+        </button>
+      )}
+    </>
+  );
+};
 
 export default App;
